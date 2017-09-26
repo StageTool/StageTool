2efea86fd1dee73ddcdb147f662120692a3e8b38
Georgi Atanasov <gaterin98@gmail.com>
2017-09-26 09:15:14 +0200

    sprint 1

--- StageTool/src/app/app.module.ts
+++ StageTool/src/app/app.module.ts
@@ -1,16 +1,19 @@
+import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
-
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { PagePage } from '../pages/page/page';
+import { Page2Page } from '../pages/page2/page2';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    PagePage,
+    Page2Page
   ],
   imports: [
     BrowserModule,
@@ -19,7 +22,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    PagePage,
+    Page2Page
   ],
   providers: [
     StatusBar,
