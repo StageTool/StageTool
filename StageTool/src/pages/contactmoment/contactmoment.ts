449eb10989df495c67d8dbe63c20de1649f0bebf
FrankKrijnen <frank.krijnen@hotmail.com>
2017-10-03 10:42:34 +0200

    beter

--- StageTool/src/pages/contactmoment/contactmoment.ts
+++ StageTool/src/pages/contactmoment/contactmoment.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController, ActionSheetController } from 'ionic-angular';
-import { AngularFireModule} from 'angularfire2';
-import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
 /**
  * Generated class for the ContactmomentPage page.
  *
@@ -14,30 +13,13 @@ import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/databas
   selector: 'page-contactmoment',
   templateUrl: 'contactmoment.html',
 })
-
 export class ContactmomentPage {
-message: FirebaseListObservable<any>;
-  constructor(public navCtrl: NavController, af:AngularFireDatabase, public alertCtrl:AlertController) {
-    this.message = af.list("/message");
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ContactmomentPage');
-  }
-  opslaan(){
-    this.alertCtrl.create({
-      buttons: [
-        ,
-        {
-          text: "opslaan",
-          handler: data => {
-            this.message.push({
-              title: data.title
-            });
-          }
-        }
-      ]
-    })
   }
 
 }
