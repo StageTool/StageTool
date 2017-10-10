bcbc4198a03bc789707394be0d1651120dd5bf6f
haik-4a5a <460252@edu.rocmn.nl>
2017-10-10 09:37:44 +0200

    Opslaan is nu mogelijk

--- StageTool/src/pages/contactmoment/contactmoment.ts
+++ StageTool/src/pages/contactmoment/contactmoment.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { ToastController } from 'ionic-angular';
 
 /**
  * Generated class for the ContactmomentPage page.
@@ -14,12 +16,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'contactmoment.html',
 })
 export class ContactmomentPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  invoer: FirebaseListObservable<any>;
+  data = { cvorm: " ", date: " ", text: " "};
+  constructor(public navCtrl: NavController, public navParams: NavParams, public af: AngularFireDatabase, public toastCtrl: ToastController) {
+    this.invoer = af.list('/invoer');
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad ContactmomentPage');
+    console.log('ionViewDidLoad FormulierPage');
   }
 
+
+
+
+  SaveData() {
+    this.invoer.push({
+      cvorm: this.data.cvorm,
+      date: this.data.cvorm,
+      text: this.data.text
+
+
+    })
+  }
+
+  presentToast() {
+    const toast = this.toastCtrl.create({
+      message: 'Contact moment opgeslagen',
+      duration: 3000,
+      position: 'top'
+    });
+     toast.present();
+  }
 }
