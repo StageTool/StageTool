1811233de5ee51d924f8b8eceef3e19f8cbac9f0
haik-4a5a <460252@edu.rocmn.nl>
2017-10-12 10:51:14 +0200

    alles weer goed

--- StageTool/src/pages/home/home.ts
+++ StageTool/src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { ContactmomentPage } from '../contactmoment/contactmoment';
 
 @Component({
   selector: 'page-home',
@@ -9,9 +8,7 @@ import { ContactmomentPage } from '../contactmoment/contactmoment';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+
   }
-  goToContactmoment(params){
-    if (!params) params = {};
-    this.navCtrl.setRoot(ContactmomentPage);
-  }
+
 }
